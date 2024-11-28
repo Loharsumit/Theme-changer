dad8883868836c69d7e57486164bd131103b0298
SUMIT LOHAR <94884266+Loharsumit@users.noreply.github.com>
2024-11-28 16:22:59 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  //Added darkMode Confiig
+  darkMode: "class",
+
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
